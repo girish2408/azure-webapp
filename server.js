c33c5f130fc4b9b95690949b7c58c4995e2a5a54
Girish Watwani <girishwatwani@Girishs-MacBook-Air.local>
2021-05-19 21:47:00 -0400

    removed file

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end('Hello World');
 });
-console.log(process.env.port);
-const port = process.env.port || 1337;
+console.log(process.env.PORT);
+const port = process.env.PORT || 1337;
 
 server.listen(port);
 console.log('server running at port ' , port);
