27560e0ebff4b1bee1a0dd6753a352e1fa7d0f88
Girish Watwani <girishwatwani@Girishs-MacBook-Air.local>
2021-05-19 22:01:14 -0400

    ousg

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('hello world');
 })
