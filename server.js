d6b47646269964f52dccdd0f29c00c17d6da9d3b
Girish Watwani <girishwatwani@Girishs-MacBook-Air.local>
2021-05-23 11:22:22 -0400

    added swagger

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const  bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
+app.use('/',swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+// app.get('/', (req, res) => {
+//   res.send(`Helloo World from node.js and port number is ${port}`);
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
